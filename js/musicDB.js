5e18b3f524923d8d360d21b65d92cdea0b41a78d
vcstar <vcstar123@yandex.com>
2021-06-14 00:18:41 +0800

    Newest001

---
+++ js/musicDB.js
@@ -0,0 +1,87 @@
+//name		歌曲名称			是
+//artist	艺术家数组或文本	否
+//cover		封面链接			否
+//lyrics	歌词文本			否
+//provider	提供器名称			否，默认 default
+//lrc		歌词链接			否
+window.QPlayer.list = [
+	{
+        "name": "且听风吟",
+        "artist": "V.A.",
+        "audio": "https://cdn.jsdelivr.net/gh/moeshin/QPlayer-res/且听风吟.mp3"
+    },
+    {
+        "name": "白色",
+        "artist": "灰澈",
+        "audio": "https://cdn.jsdelivr.net/gh/moeshin/QPlayer-res/白色.mp3",
+        "cover": "https://cdn.jsdelivr.net/gh/moeshin/QPlayer-res/白色.jpg"
+    },
+    {
+        "name": "渚 Warm Piano Arrange",
+        "artist": "Key Sounds Label",
+        "audio": "https://cdn.jsdelivr.net/gh/moeshin/QPlayer-res/渚 Warm Piano Arrange.mp3",
+        "cover": "https://cdn.jsdelivr.net/gh/moeshin/QPlayer-res/渚 Warm Piano Arrange.jpg"
+    },
+    {
+        "name": "小さなてのひら",
+        "artist": "水月陵",
+        "audio": "https://cdn.jsdelivr.net/gh/moeshin/QPlayer-res/小さなてのひら.mp3",
+        "cover": "https://cdn.jsdelivr.net/gh/moeshin/QPlayer-res/小さなてのひら.jpg"
+    },
+    {
+        "name": "Nightglow",
+        "artist": "蔡健雅",
+        "audio": "https://cdn.jsdelivr.net/gh/moeshin/QPlayer-res/Nightglow.mp3",
+        "cover": "https://cdn.jsdelivr.net/gh/moeshin/QPlayer-res/Nightglow.jpg",
+        "lrc": "https://cdn.jsdelivr.net/gh/moeshin/QPlayer-res/Nightglow.lrc"
+    },
+    {
+        "name": "やわらかな光",
+        "artist": "やまだ豊",
+        "audio": "https://cdn.jsdelivr.net/gh/moeshin/QPlayer-res/やわらかな光.mp3",
+        "cover": "https://cdn.jsdelivr.net/gh/moeshin/QPlayer-res/やわらかな光.jpg"
+    },
+    {
+        "name": "Saya's Song",
+        "artist": "Lia",
+        "audio": "https://cdn.jsdelivr.net/gh/moeshin/QPlayer-res/Saya's Song.mp3",
+        "cover": "https://cdn.jsdelivr.net/gh/moeshin/QPlayer-res/Saya's Song.jpg",
+        "lrc": "https://cdn.jsdelivr.net/gh/moeshin/QPlayer-res/Saya's Song.lrc"
+    },
+    {
+        "name": "一番の宝物 (Original Version)",
+        "artist": "karuta",
+        "audio": "https://cdn.jsdelivr.net/gh/moeshin/QPlayer-res/一番の宝物 (Original Version).mp3",
+        "cover": "https://cdn.jsdelivr.net/gh/moeshin/QPlayer-res/一番の宝物 (Original Version).jpg",
+        "lrc": "https://cdn.jsdelivr.net/gh/moeshin/QPlayer-res/一番の宝物 (Original Version).lrc"
+    },
+    {
+        "name": "僕が死のうと思ったのは",
+        "artist": "中島美嘉",
+        "audio": "https://cdn.jsdelivr.net/gh/moeshin/QPlayer-res/僕が死のうと思ったのは.mp3",
+        "cover": "https://cdn.jsdelivr.net/gh/moeshin/QPlayer-res/僕が死のうと思ったのは.jpg",
+        "lrc": "https://cdn.jsdelivr.net/gh/moeshin/QPlayer-res/僕が死のうと思ったのは.lrc"
+    },
+    {
+        "name": "跟太阳系说再见",
+        "artist": "祝乾亮",
+        "audio": "https://cdn.jsdelivr.net/gh/moeshin/QPlayer-res/跟太阳系说再见.mp3",
+        "cover": "https://cdn.jsdelivr.net/gh/moeshin/QPlayer-res/跟太阳系说再见.jpg"
+    },
+    {
+        "name": "I Love Study 我爱学习（Off Vocal）",
+        "artist": "秋风MusiX",
+        "audio": "https://cdn.jsdelivr.net/gh/moeshin/QPlayer-res/I Love Study 我爱学习（Off Vocal）.mp3",
+        "cover": "https://cdn.jsdelivr.net/gh/moeshin/QPlayer-res/I Love Study 我爱学习（Off Vocal）.jpg"
+    },
+    {
+        "name": "Blessing 世界版",
+        "artist": [
+            "茶理理",
+            "V.A."
+        ],
+        "audio": "https://cdn.jsdelivr.net/gh/moeshin/QPlayer-res/Blessing 世界版.mp3",
+        "cover": "https://cdn.jsdelivr.net/gh/moeshin/QPlayer-res/Blessing 世界版.jpg",
+        "lrc": "https://cdn.jsdelivr.net/gh/moeshin/QPlayer-res/Blessing 世界版.lrc"
+    }
+];
---
+++ js/musicUI.js
@@ -0,0 +1,44 @@
+//播放器初始化
+window.q = window.QPlayer;
+window.$ = q.$;
+$(function () {
+	//播放器显示
+    $('#QPlayer').addClass('QPlayer-show');
+    //$('#QPlayer-more').addClass('QPlayer-list-show QPlayer-lyrics-show');
+	$('#QPlayer-more').addClass('QPlayer-list-show');
+	//$('#QPlayer-more').addClass('');
+    $('#js').val($('#js-code').text());
+});
+
+
+//播放器切换
+function switchMusicMode(){
+    var music = document.cookie.replace(/(?:(?:^|.*;\s*)music\s*\=\s*([^;]*).*$)|^.*$/, "$1") || '0';
+    if(music == '0'){
+        //document.body.classList.add('music');
+		window.q = window.QPlayer;
+		window.$ = q.$;
+		$(function () {
+		$('#QPlayer').removeClass('QPlayer-show');
+		//$('#QPlayer-more').addClass('QPlayer-list-show QPlayer-lyrics-show');
+		$('#QPlayer-more').addClass('QPlayer-list-show');
+		//$('#QPlayer-more').addClass('');
+		$('#js').val($('#js-code').text());
+		});
+        document.cookie = "music=1;path=/"
+    }else{
+        //document.body.classList.remove('music');
+		window.q = window.QPlayer;
+		window.$ = q.$;
+		$(function () {
+		//播放器显示
+		$('#QPlayer').addClass('QPlayer-show');
+		//$('#QPlayer-more').addClass('QPlayer-list-show QPlayer-lyrics-show');
+		$('#QPlayer-more').addClass('QPlayer-list-show');
+		//$('#QPlayer-more').addClass('');
+		$('#js').val($('#js-code').text());
+		});
+        document.cookie = "music=0;path=/"
+    }
+
+}
