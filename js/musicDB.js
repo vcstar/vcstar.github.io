77d7395f5ded9e13ef59af96a2e444dba4768273
vcstar <53456917+vcstar@users.noreply.github.com>
2022-08-22 08:47:50 +0800

    New002

--- js/musicDB.js
+++ js/musicDB.js
@@ -5,15 +5,65 @@
 //provider	提供器名称			否，默认 default
 //lrc		歌词链接			否
 window.QPlayer.list = [
-	{
-        "name": "Uptown Funk",
-        "artist": "Mark Ronson",
-        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/Mark Ronson_Uptown Funk.mp3"
+    {
+        "name": "Louder",
+        "artist": "Charice",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/Charice_Louder.mp3",
     },
     {
-        "name": "童话镇",
-        "artist": "陈一发儿",
-        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/陈一发儿_童话镇.mp3",
+        "name": "Hotel California",
+        "artist": "Eagles",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/Eagles_Hotel California.mp3",
+    },
+    {
+        "name": "I Hate Myself For Loving You",
+        "artist": "Joan Jett",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/Joan Jett_I Hate Myself For Loving You.mp3",
+    },
+    {
+        "name": "Uptown Funk",
+        "artist": "Mark Ronson",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/Mark Ronson_Uptown Funk.mp3",
+    },
+    {
+        "name": "Beat It",
+        "artist": "Michael Jackson",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/Michael Jackson_Beat It.mp3",
+    },
+    {
+        "name": "Brother Louie",
+        "artist": "Modern Talking",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/Modern Talking_Brother Louie.mp3",
+    },
+    {
+        "name": "เจอกับตัวเอง...ถึงรู้",
+        "artist": "Sara Nusara Poongprasert",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/Sara Nusara Poongprasert_เจอกับตัวเอง...ถึงรู้.mp3",
+    },
+    {
+        "name": "恋人未满",
+        "artist": "SHE",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/SHE_恋人未满.mp3",
+    },
+    {
+        "name": "浪人情歌",
+        "artist": "伍佰",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/伍佰_浪人情歌.mp3",
+    },
+    {
+        "name": "世界第一等",
+        "artist": "刘德华",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/刘德华_世界第一等.mp3",
+    },
+    {
+        "name": "信仰",
+        "artist": "张信哲",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/张信哲_信仰.mp3",
+    },
+    {
+        "name": "饿狼传说",
+        "artist": "张学友",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/张学友_饿狼传说.mp3",
     },
     {
         "name": "护花使者",
@@ -21,23 +71,88 @@ window.QPlayer.list = [
         "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/李克勤_护花使者.mp3",
     },
     {
+        "name": "执迷不悔",
+        "artist": "王菲",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/王菲_执迷不悔.mp3",
+    },
+    {
+        "name": "笑忘书",
+        "artist": "王菲",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/王菲_笑忘书.mp3",
+    },
+    {
+        "name": "月满西楼",
+        "artist": "童丽",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/童丽_月满西楼.mp3",
+    },
+    {
+        "name": "耶利亚女郎",
+        "artist": "童安格",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/童安格_耶利亚女郎.mp3",
+    },
+    {
+        "name": "光阴的故事",
+        "artist": "罗大佑",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/罗大佑_光阴的故事.mp3",
+    },
+    {
+        "name": "失恋",
+        "artist": "草蜢",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/草蜢_失恋.mp3",
+    },
+    {
+        "name": "许愿池的希腊少女",
+        "artist": "蔡依林",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/蔡依林_许愿池的希腊少女.mp3",
+    },
+    {
+        "name": "故乡",
+        "artist": "许巍",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/许巍_故乡.mp3",
+    },
+    {
         "name": "城里的月光",
         "artist": "许美静",
         "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/许美静_城里的月光.mp3",
     },
-	{
+    {
+        "name": "都是夜归人",
+        "artist": "许美静",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/许美静_都是夜归人.mp3",
+    },
+    {
+        "name": "Andy",
+        "artist": "阿杜",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/阿杜_Andy.mp3",
+    },
+    {
+        "name": "天黑",
+        "artist": "阿杜",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/阿杜_天黑.mp3",
+    },
+    {
+        "name": "撕夜",
+        "artist": "阿杜",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/阿杜_撕夜.mp3",
+    },
+    {
+        "name": "童话镇",
+        "artist": "陈一发儿",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/陈一发儿_童话镇.mp3",
+    },
+    {
+        "name": "梦醒时分",
+        "artist": "陈淑华",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/陈淑华_梦醒时分.mp3",
+    },
+    {
+        "name": "伤感的恋人",
+        "artist": "黄凯芹",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/黄凯芹_伤感的恋人.mp3",
+    },
+    {
         "name": "无地自容",
         "artist": "黑豹乐队",
         "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/黑豹乐队_无地自容.mp3",
     },
-    {
-        "name": "Louder",
-        "artist": "Charice",
-        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/Charice_Louder.mp3",
-    },
-    {
-        "name": "信仰",
-        "artist": "张信哲",
-        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/张信哲_信仰.mp3",
-    }
 ];
